a55a2fe53e6e7b0d1321f670727587b15872af07
VivianOverbey <voverbey@mail.csuchico.edu>
2023-03-27 07:30:19 -0700

    PlayerEmissionParticles + Water Shader

--- Frogs-Of-Rage/Assets/Programming/Scripts/VFXScripts/PlayerEmissionVFX.cs
+++ Frogs-Of-Rage/Assets/Programming/Scripts/VFXScripts/PlayerEmissionVFX.cs
@@ -6,43 +6,51 @@ using UnityEngine.Rendering;
 
 public class PlayerEmissionVFX : MonoBehaviour
 {
-    [SerializeField] private Renderer meshRenderer;
+    [SerializeField] private Renderer _meshRenderer;
 
-    [SerializeField] float _effectDuration;
-    [SerializeField] float _particleQuantity;
-    [SerializeField] float _particleSpeed;
+    #region "Variables"
+    [SerializeField] float _effectDuration = 2;
+    [SerializeField] float _matFlashDuration = 1.25f;
+
+    [SerializeField] float _particleQuantity = 25;
+    [SerializeField] float _particleSpeed = 5;
+
 
     [Header("Damage Sprite")]
     [Space(5)]
     [SerializeField] private Sprite _damageSprite;
-    [SerializeField] private float _damageSpriteScale;
+    [SerializeField] private Color _damageMatColor = Color.red;
+    [SerializeField] private float _damageSpriteScale = 0.15f;
     [Header("Electrocuted Sprite")]
     [Space(5)]
     [SerializeField] private Sprite _electrocutedSprite;
-    [SerializeField] private float _electrocutedSpriteScale;
+    [SerializeField] private Color _electrocutedMatColor = Color.black;
+    [SerializeField] private float _electrocutedSpriteScale =0.1f;
     [Header("Heal Sprite")]
     [Space(5)]
     [SerializeField] private Sprite _healSprite;
-    [SerializeField] private float _healSpriteScale;
-    [Header("Stun Sprite")]
-    [Space(5)]
-    [SerializeField] private Sprite _stunSprite;
-    [SerializeField] private float _stunSpriteScale;
+    [SerializeField] private Color _healMatColor = Color.green;
+    [SerializeField] private float _healSpriteScale = 0.15f;
+
+    #endregion
 
     private Transform _particlePool;
     private SpriteRenderer[] _particleRenderers;
     private Coroutine _particleCoroutine;
+
+    private Material[] _playerMaterials;
+    private Color[] _initialColors;
 
     public enum EmitPlayerParticlesMode
     {
         Damage,
         Electrocuted,
         Heal,
-        Stun
     }
 
     private void Start()
     {
+        GetPlayerMat();
         SetUpParticles();
     }
 
@@ -51,16 +59,13 @@ public class PlayerEmissionVFX : MonoBehaviour
         switch (mode)
         {
             case EmitPlayerParticlesMode.Damage:
-                SpawnParticles(_damageSpriteScale, _damageSprite);
+                SpawnParticles(_damageSpriteScale, _damageSprite, _damageMatColor);
                 break;
             case EmitPlayerParticlesMode.Electrocuted:
-                SpawnParticles(_electrocutedSpriteScale, _electrocutedSprite);
+                SpawnParticles(_electrocutedSpriteScale, _electrocutedSprite, _electrocutedMatColor);
                 break;
             case EmitPlayerParticlesMode.Heal:
-                SpawnParticles(_healSpriteScale, _healSprite);
-                break;
-            case EmitPlayerParticlesMode.Stun:
-                SpawnParticles(_stunSpriteScale, _stunSprite);
+                SpawnParticles(_healSpriteScale, _healSprite, _healMatColor);
                 break;
             default:
                 break;
@@ -68,9 +73,9 @@ public class PlayerEmissionVFX : MonoBehaviour
     }
 
 
-    private void SpawnParticles(float particleScale, Sprite sprite)
+    private void SpawnParticles(float particleScale, Sprite sprite, Color matColor)
     {
-        if (!meshRenderer || !_particlePool || _particleCoroutine != null) return;
+        if (!_meshRenderer || !_particlePool || _particleCoroutine != null) return;
 
         Mesh objectMesh = null;
         List<Vector3> vertexPositions = new List<Vector3>();
@@ -78,15 +83,15 @@ public class PlayerEmissionVFX : MonoBehaviour
 
         SetParticleSprites(sprite);
 
-        if(meshRenderer as SkinnedMeshRenderer) // gets skinned renderer if the render is that type so it can use a baked mesh for proper information
+        if(_meshRenderer as SkinnedMeshRenderer) // gets skinned renderer if the render is that type so it can use a baked mesh for proper information
         {
-            SkinnedMeshRenderer skinnedRenderer = meshRenderer as SkinnedMeshRenderer;
+            SkinnedMeshRenderer skinnedRenderer = _meshRenderer as SkinnedMeshRenderer;
 
             skinnedRenderer.BakeMesh(objectMesh, true);
             objectMesh.GetVertices(vertexPositions);
             objectMesh.GetNormals(vertexNormals);
         }    
-        else if (meshRenderer.gameObject.TryGetComponent(out MeshFilter mFilter)) //if its not a skinned mesh, finds the filter and used that mesh
+        else if (_meshRenderer.gameObject.TryGetComponent(out MeshFilter mFilter)) //if its not a skinned mesh, finds the filter and used that mesh
         {
             objectMesh = mFilter.mesh;
             objectMesh.GetVertices(vertexPositions);
@@ -108,7 +113,7 @@ public class PlayerEmissionVFX : MonoBehaviour
         {
             indexer = Random.Range(0, vertexPositions.Count);
 
-            particleSpawnPos = meshRenderer.transform.localToWorldMatrix.MultiplyPoint3x4(vertexPositions[indexer]);
+            particleSpawnPos = _meshRenderer.transform.localToWorldMatrix.MultiplyPoint3x4(vertexPositions[indexer]);
             particleMoveDirection = Vector3.Normalize(vertexNormals[indexer]);
 
             currentObject = _particlePool.GetChild(i).gameObject;
@@ -133,14 +138,22 @@ public class PlayerEmissionVFX : MonoBehaviour
 
         if(_particleCoroutine == null)
         {
-            _particleCoroutine = StartCoroutine(VFXTimer());
+            for (int i = 0; i < _playerMaterials.Length; i++)
+            {
+                _playerMaterials[i].color = _initialColors[i];
+            }
+
+            _particleCoroutine = StartCoroutine(VFXTimer(matColor));
         }
 
     }
 
-    private IEnumerator VFXTimer()
+    private IEnumerator VFXTimer(Color matColor)
     {
         Vector3 startingScale = Vector3.zero;
+
+        StartCoroutine(FlashPlayerMat(matColor));
+
         if (_particlePool.childCount > 0)
         {
             startingScale = _particlePool.GetChild(0).transform.localScale;
@@ -185,12 +198,12 @@ public class PlayerEmissionVFX : MonoBehaviour
 
     private void SetUpParticles()
     {
-        if (!meshRenderer) return;
+        if (!_meshRenderer) return;
 
 
         _particlePool = new GameObject().transform;
         _particlePool.name = "Damage Particle Pool";
-        _particlePool.parent = meshRenderer.gameObject.transform;
+        _particlePool.parent = _meshRenderer.gameObject.transform;
 
         Transform particleObject;
         List<SpriteRenderer> spriteRenderers = new List<SpriteRenderer>();
@@ -206,23 +219,62 @@ public class PlayerEmissionVFX : MonoBehaviour
         _particleRenderers = spriteRenderers.ToArray();
     }
 
-    private void Update()
+    private void GetPlayerMat()
     {
-        if(Input.GetKeyDown("1"))
+        if(_meshRenderer)
         {
-            EmitPlayerParticles(EmitPlayerParticlesMode.Damage);
-        }
-        if (Input.GetKeyDown("2"))
-        {
-            EmitPlayerParticles(EmitPlayerParticlesMode.Electrocuted);
-        }
-        if (Input.GetKeyDown("3"))
-        {
-            EmitPlayerParticles(EmitPlayerParticlesMode.Heal);
-        }
-        if (Input.GetKeyDown("4"))
-        {
-            EmitPlayerParticles(EmitPlayerParticlesMode.Stun);
+            _playerMaterials = _meshRenderer.materials;
+            _initialColors = new Color[_playerMaterials.Length];
+
+            for (int i = 0; i < _initialColors.Length; i++)
+            {
+                _initialColors[i] = _playerMaterials[i].color;            }
         }
     }
+    private IEnumerator FlashPlayerMat(Color flashColor)
+    {
+        bool color = false;
+        for (float t = 0; t < _matFlashDuration; t += 0.15f)
+        {
+            color = !color;
+
+            if(color)
+            {
+                for (int i = 0; i < _playerMaterials.Length; i++)
+                {
+                    _playerMaterials[i].color = flashColor;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < _playerMaterials.Length; i++)
+                {
+                    _playerMaterials[i].color = _initialColors[i];
+                }
+            }
+
+            yield return new WaitForSeconds(0.15f);
+        }
+
+        for (int i = 0; i < _playerMaterials.Length; i++)
+        {
+            _playerMaterials[i].color = _initialColors[i];
+        }
+    }
+
+
+    public static void AddPlayerEmissionVFX(GameObject objectToAddTo, Renderer objectRenderer, Sprite damageSprite, Sprite electrocutedSprite, Sprite healSprite)
+    {
+        PlayerEmissionVFX newEmissionVFX = objectToAddTo.AddComponent<PlayerEmissionVFX>();
+
+        newEmissionVFX._damageSprite = damageSprite;
+        newEmissionVFX._electrocutedSprite = electrocutedSprite;
+        newEmissionVFX._healSprite = healSprite;
+
+        newEmissionVFX._meshRenderer = objectRenderer;
+
+        newEmissionVFX.GetPlayerMat();
+        newEmissionVFX.SetUpParticles();
+    }
+
 }

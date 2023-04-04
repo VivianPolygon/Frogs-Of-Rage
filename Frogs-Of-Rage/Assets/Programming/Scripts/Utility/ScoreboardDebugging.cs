297f75a43bdd53fcc7651a6d5a7e425f70d398be
VivianOverbey <voverbey@mail.csuchico.edu>
2023-04-03 17:29:06 -0700

    Leaderboard 1
    A single leaderboard and it's inputs work

---
+++ Frogs-Of-Rage/Assets/Programming/Scripts/Utility/ScoreboardDebugging.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreboardDebugging : MonoBehaviour
+{
+    public PlayerController _playerController;
+
+    private void OnGUI()
+    {
+
+        if(_playerController)
+        {
+            if(GUILayout.Button("PlayerWinTrigger"))
+            {
+                PlayerController.OnPlayerWin(new PlayerWinEventArgs(GameManager.Instance.gameTimer, _playerController.playerData));
+            }
+        }
+
+        if(GameManager.Instance)
+        {
+            if(GameManager.Instance.gameTimer)
+            {
+                if(GUILayout.Button("ResetGameTimer"))
+                {
+                    GameManager.Instance.gameTimer.totalTime = 0;
+                }
+
+                GUI.color = Color.black;
+                GUILayout.Label("Current Game Timer Time: <b>" +  UtilityFunctions.ConvertSecondsToStandardTimeFormatString(GameManager.Instance.gameTimer.totalTime) + "</b>");
+            }
+        }
+
+    }
+}

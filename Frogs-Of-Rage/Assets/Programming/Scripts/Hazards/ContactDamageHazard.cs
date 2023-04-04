016e1c5a257eaf4ef3e085eb0778ead96777b30d
CSmith175 <98501848+CSmith175@users.noreply.github.com>
2023-04-04 09:51:03 -0700

    Working on hot sauce hazard

---
+++ Frogs-Of-Rage/Assets/Programming/Scripts/Hazards/ContactDamageHazard.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContactDamageHazard : Hazard
+{
+    public float damagePerSecond = 1f;
+    private GameManager gameManager;
+    private bool playerTouching = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameManager.Instance;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerTouching = true;
+            InvokeDamage();
+            StartCoroutine(DamagePlayer());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (playerTouching)
+            playerTouching = !playerTouching;
+    }
+
+    private IEnumerator DamagePlayer()
+    {
+        gameManager.playerController.curHealth -= damagePerSecond;
+        yield return new WaitForSeconds(1f);
+        if(playerTouching)
+            StartCoroutine(DamagePlayer());
+    }
+}

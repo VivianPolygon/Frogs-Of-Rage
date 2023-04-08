f87183e99a9c0c5324e6857ebdfd8f9c0aeb53cd
Rein Tice <rctice@mail.csuchico.edu>
2023-04-08 16:19:32 -0700

    poster and hat spin

--- Frogs-Of-Rage/Assets/Programming/Scripts/EnableOnTriggerEnterWithTag.cs
+++ Frogs-Of-Rage/Assets/Programming/Scripts/EnableOnTriggerEnterWithTag.cs
@@ -2,14 +2,38 @@ using UnityEngine;
 
 public class EnableOnTriggerEnterWithTag : MonoBehaviour
 {
+    public PosterController posterController;
     [SerializeField] private string targetTag = "YourTargetTag";
     [SerializeField] private GameObject objectToEnable;
+    public bool hasTriggered = false;
+
+    private void Start()
+    {
+        if (posterController == null)
+        {
+            Debug.LogError("PosterController is not assigned!");
+        }
+        hasTriggered = false;
+    }
+
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag(targetTag))
+        Debug.Log("Triggered by: " + other.gameObject.name);
+
+        if (other.CompareTag(targetTag) && !hasTriggered)
         {
             objectToEnable.SetActive(true);
+            hasTriggered = true;
+            if (posterController != null)
+            {
+                posterController.HasTriggered = true;
+            }
+            else
+            {
+                Debug.LogError("PosterController is not assigned!");
+            }
         }
     }
+
 }

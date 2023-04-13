593429905592ce1682cf325156496a84caada06f
VivianOverbey <voverbey@mail.csuchico.edu>
2023-04-13 00:01:53 -0700

    tools fix 4

--- Frogs-Of-Rage/Assets/Programming/Scripts/Hats/HatDatabase.cs
+++ Frogs-Of-Rage/Assets/Programming/Scripts/Hats/HatDatabase.cs
@@ -37,8 +37,9 @@ public class HatDatabase : ScriptableObject
                 Debug.LogWarning("Hat Database is empty");
                 return new List<HatData>();
             }
-        } 
-        
+        }
+
+#if UNITY_EDITOR 
         set
         {
             if(HatsEditor.WindowOpen) //can only write if the hats editor window is open. its not foolproof, but should prevent unauthorized writing
@@ -50,6 +51,7 @@ public class HatDatabase : ScriptableObject
                 Debug.LogWarning("The Hat database should only be modified through the editor window. nothing was written");
             }
         }
+#endif
     }
 
 

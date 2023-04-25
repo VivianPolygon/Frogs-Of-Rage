fcda0d5f65695da64910c98683e84ccfff8247a3
Christian Smith <cmsmith2@mail.csuchico.edu>
2023-04-24 17:51:33 -0700

    Finished bottle rocket

--- Frogs-Of-Rage/Assets/Programming/Scripts/Player/TriggerOpeningExit.cs
+++ Frogs-Of-Rage/Assets/Programming/Scripts/Player/TriggerOpeningExit.cs
@@ -5,29 +5,42 @@ using UnityEngine;
 public class TriggerOpeningExit : MonoBehaviour
 {
     public float waitTimeBeforeAnimation = 3f;
+    public AudioClip launchSFX, fuseSFX;
     public GameObject sparksVFX;
     public GameObject explosionVFX;
 
+    private AudioSource audioSource;
+    private bool activated = false;
+
     private void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         sparksVFX.SetActive(false);
         explosionVFX.SetActive(false);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player") && GameManager.Instance.playerController.secondaryObjectiveComplete)
-            StartCoroutine(TriggerObjectiveAnimation(other));
+        if (!activated)
+        {
+            if (other.gameObject.CompareTag("Player") && GameManager.Instance.playerController.secondaryObjectiveComplete)
+            {
+                StartCoroutine(TriggerObjectiveAnimation(other));
+                activated = true;
+            }
+        }
     }
 
     private IEnumerator TriggerObjectiveAnimation(Collider other)
     {
         sparksVFX.SetActive(true);
+        audioSource.PlayOneShot(fuseSFX);
         yield return new WaitForSeconds(waitTimeBeforeAnimation);
 
         GameObject.FindGameObjectWithTag("Exit").GetComponent<Collider>().enabled = true;
         GetComponent<Animator>().SetTrigger("ObjectiveComplete");
 
         yield return new WaitForSeconds(.3f);
+        audioSource.PlayOneShot(launchSFX);
     }
 }
--- Frogs-Of-Rage/Assets/Programming/Scripts/Props/Ventogic.cs
+++ Frogs-Of-Rage/Assets/Programming/Scripts/Props/Ventogic.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class Ventogic : MonoBehaviour
 {
+    public AudioClip explosionSFX;
+    public GameObject explosionVFX;
+    public Vector3 offset;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter(Collider collider)
     {
         if(collider.gameObject.name == "BottleRocket")
         {
+            audioSource.PlayOneShot(explosionSFX);
+            GameObject vfx = Instantiate(explosionVFX);
+            vfx.transform.position = offset;
+            vfx.SetActive(true);
             transform.GetChild(0).gameObject.SetActive(true);
             transform.GetChild(1).gameObject.SetActive(false);
             RenderSettings.fog = true;
